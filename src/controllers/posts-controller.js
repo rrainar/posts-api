5274895933f0f0da369a6ba983154572088e9af0
Rainar Razumovski <rainar.razumovski@pipedrive.com>
2022-07-29 16:39:38 +0300

    update

--- src/controllers/posts-controller.js
+++ src/controllers/posts-controller.js
@@ -1,24 +1,36 @@
 const postsModel = require("../models/posts-model");
 
 function getAllPosts(ctx) {
-    ctx.body = postsModel.posts;
+  ctx.body = postsModel.posts;
 }
-    
-    function getById(ctx)  {
-        const index = ctx.request.params.id;
-        ctx.body = postsModel.posts[index];
-    }
-    
-    function deleteById(ctx){
-        const index = ctx.request.params.id;
-        postsModel.posts.splice(index, 1);
-        ctx.status = 204;
-    }
-    
-    function create(ctx) {
-        const text = ctx.request.body.text;
-        postsModel.posts.push({ text });
-        ctx.status = 201;
-    }
 
-    module.exports = { getAllPosts, getById, deleteById, create };
+function getById(ctx) {
+  const index = ctx.request.params.id;
+
+    if(Number(index) > postsModel.posts.length - 1) {
+    ctx.status = 404;
+    return;
+  }
+
+  ctx.body = postsModel.posts[index];
+}
+
+function deleteById(ctx) {
+  const index = ctx.request.params.id;
+  postsModel.posts.splice(index, 1);
+  ctx.status = 204;
+}
+
+function create(ctx) {
+  const text = ctx.request.body.text;
+
+  if (!text || text === "") {
+    ctx.status = 400;
+    return;
+  }
+
+  postsModel.posts.push({ text });
+  ctx.status = 201;
+}
+
+module.exports = { getAllPosts, getById, deleteById, create };
