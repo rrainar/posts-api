6b0313902709e6ff9ad8b0aa67332490b1b80f4f
Rainar Razumovski <rainar.razumovski@pipedrive.com>
2022-07-29 13:57:35 +0300

    seed data added

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Koa = require ("koa");
 const app = new Koa();
 
-//const routes = require('./lib/routes/posts-routes');
-
-//app.use(router.routes());
+const router = require("./src/routes/posts-router");
+app.use(router.routes());
 
 app.listen(3000, () => 
 console.log("Server is running at http://localhost:3000")
--- src/models/posts-model.js
+++ src/models/posts-model.js
@@ -1,3 +1,7 @@
-const posts = [];
+const posts = [
+  { text: 'Batman vs Superman is 10/10, change my mind' },
+  { text: 'power of cats, by Catman' },
+  { text: 'Tallinn is the new Miami. Soon Moon?' }
+];
 
 module.exports = { posts };
